03737f6aa526c566fc1afc7f5c610b5ad1836106
Amit Kriplani <contact@amitkriplani.com>
2013-09-19 09:20:41 +0530

    Autoload, IrisUtility, start Bootstrap

--- app/Iris.php
+++ app/Iris.php
@@ -1,53 +1,58 @@
 <?php
 /**
+ *
  * @package iris
  * @subpackage main
- * @version 1a
- * @name iris 
- * 
+ * @name iris
+ *      
  * @author amit kriplani <contact@amitkriplani.com>
  */
 class Iris {
-	
 	protected static $_instance;
-	
 	
 	/**
 	 * Save mode environment for easy access
-	 * 
+	 *
 	 * @var String
 	 */
 	protected static $_mode = 'live';
 	
 	/**
+	 *
 	 * @return the $_instance
 	 */
 	public static function getInstance() {
-		if (!Iris::$_instance)
-			Iris::$_instance = new Iris;
+		if (! Iris::$_instance)
+			Iris::$_instance = new Iris ();
 		return Iris::$_instance;
 	}
-
+	
 	/**
+	 *
 	 * @return the $_mode
 	 */
 	public static function getMode() {
 		return Iris::$_mode;
 	}
-
+	
 	/**
-	 * @param string $_mode
+	 *
+	 * @param string $_mode        	
 	 */
 	protected static function setMode($_mode) {
 		Iris::$_mode = $_mode;
 	}
-
+	
 	/**
 	 * Constructor of the main class checks for all pre-requisites.
 	 *
 	 * @return void
 	 */
 	protected function __construct() {
+		if (( float ) PHP_VERSION < 5.3)
+			throw new Exception ( 'Iris requires PHP version 5.3 or above.' ); // @todo change to IrisException
+				                                                                   
+		// @todo check other requirements like internet connection and sendmail
 	}
 	
 	/**
@@ -59,8 +64,9 @@ class Iris {
 	 * @return Iris This instance
 	 */
 	public function setup($mode) {
-		Iris::setMode($mode);
-		
+		Iris::setMode ( $mode );
+		Iris::setupAutoloading ();
+		Iris::bootstrap ();
 		return $this;
 	}
 	
@@ -72,6 +78,20 @@ class Iris {
 	 * @return void
 	 */
 	public function run() {
-		die(Iris::getMode());
+		die ( Iris::getMode () );
+	}
+	protected static function setupAutoloading() {
+		require_once 'IrisUtility.php';
+		spl_autoload_register ( array (
+				IrisUtility::getInstance (),
+				'_autoload' 
+		) );
+	}
+	protected static function bootstrap() {
+		if (! Iris::getMode ())
+			throw new Exception ( 'Cannot bootstrap without "mode"' ); // @todo change to IrisException
+		
+		$bootstrap = new Bootstrap ();
+		$bootstrap->bootstrap ( Iris::getMode () );
 	}
 }
---
+++ app/IrisUtility.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @todo php doc
+ * @author amit
+ *
+ */
+class IrisUtility {
+	const DIR_NAME_CODE_CUSTOM = 'custom'; // @todo move this to configuration
+	const DIR_NAME_CODE_CONTRIB = 'contrib'; // @todo move this to configuration
+	const DIR_NAME_CODE_CORE = 'core'; // @todo move this to configuration
+	protected $_classFileExts = array ( // @todo move this to configuration
+			'.php' 
+	);
+	protected static $_instance;
+	public static function getInstance() {
+		if (! IrisUtility::$_instance)
+			IrisUtility::$_instance = new IrisUtility ();
+		return IrisUtility::$_instance;
+	}
+	protected function __construct() {
+	}
+	public function _autoload($class) {
+		foreach ( $this->_classFileExts as $ext ) {
+			
+			$file = str_replace ( '_', DIRECTORY_SEPARATOR, $class ) . $ext;
+			if (file_exists ( implode ( DIRECTORY_SEPARATOR, array (
+					APP_ROOT,
+					IrisUtility::DIR_NAME_CODE_CUSTOM,
+					$file 
+			) ) ))
+				return require_once implode ( DIRECTORY_SEPARATOR, array (
+						APP_ROOT,
+						IrisUtility::DIR_NAME_CODE_CUSTOM,
+						$file 
+				) );
+			if (file_exists ( implode ( DIRECTORY_SEPARATOR, array (
+					APP_ROOT,
+					IrisUtility::DIR_NAME_CODE_CONTRIB,
+					$file 
+			) ) ))
+				return require_once implode ( DIRECTORY_SEPARATOR, array (
+						APP_ROOT,
+						IrisUtility::DIR_NAME_CODE_CONTRIB,
+						$file 
+				) );
+			if (file_exists ( implode ( DIRECTORY_SEPARATOR, array (
+					APP_ROOT,
+					IrisUtility::DIR_NAME_CODE_CORE,
+					$file 
+			) ) ))
+				return require_once implode ( DIRECTORY_SEPARATOR, array (
+						APP_ROOT,
+						IrisUtility::DIR_NAME_CODE_CORE,
+						$file 
+				) );
+		}
+	}
+}
